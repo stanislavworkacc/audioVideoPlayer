58dbea201781f73fe36688499baea8f49372cd4a
stanislavworkacc <stanislavworkacc@gmail.com>
2020-12-21 17:03:48 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlocks = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+
+const deactivationPlayer = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlocks.forEach(item => item.classList.remove('active'));
+};
+
+
+playerBtn.forEach((btn, index) => btn.addEventListener('click', () => {
+        deactivationPlayer();
+        btn.classList.add('active');
+        playerBlocks[index].classList.add('active')
+    })
+);
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
