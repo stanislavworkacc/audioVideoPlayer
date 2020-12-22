cd8d5c1e86213e28b9212afb5194d81418a94664
stanislavworkacc <stanislavworkacc@gmail.com>
2020-12-22 18:04:19 +0300

    add videoPlayer

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,28 @@
 export const videoPlayerInit = () => {
-    console.log('hello VIDEO player')
+     const videoPlayer = document.querySelector('.video-player');
+     const videoButtonPlay = document.querySelector('.video-button__play');
+     const videoButtonStop = document.querySelector('.video-button__stop');
+     const videoTimePassed = document.querySelector('.video-time__passed');
+     const videoProgress = document.querySelector('.video-progress');
+     const videoTimeTotal = document.querySelector('.video-time__total');
+
+    const toggle = () => {
+        if(videoPlayer.paused) {
+            videoButtonPlay.classList.remove('.fa-pause');
+            videoButtonPlay.classList.add('.fa-play');
+        } else {
+            videoButtonPlay.classList.add('.fa-pause');
+            videoButtonPlay.classList.remove('.fa-play');
+
+        }
+    }
+
+     videoPlayer.addEventListener('click', () => {
+         console.log(videoPlayer.paused)
+         if(videoPlayer.paused) {
+            videoPlayer.play();
+         } else {
+            videoPlayer.pause();
+         }
+     })
 };
